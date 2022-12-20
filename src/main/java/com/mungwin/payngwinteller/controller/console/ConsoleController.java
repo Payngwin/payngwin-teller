a95514c133d03f073f7b97c07830699ba39c0df4
Nnouka Stephen <nnoukastephen@gmail.com>
2022-12-20 13:25:30 +0100

    test request to pay momo

--- src/main/java/com/mungwin/payngwinteller/controller/console/ConsoleController.java
+++ src/main/java/com/mungwin/payngwinteller/controller/console/ConsoleController.java
@@ -2,14 +2,17 @@ package com.mungwin.payngwinteller.controller.console;
 
 import com.mungwin.payngwinteller.domain.response.ApiResponse;
 import com.mungwin.payngwinteller.network.payment.clients.mtn.MoMoRestClient;
+import com.mungwin.payngwinteller.network.payment.dto.mtn.MoMoPayRequest;
+import com.mungwin.payngwinteller.network.payment.dto.mtn.MoMoPayer;
 import com.mungwin.payngwinteller.network.payment.dto.mtn.MoMoTokenDTO;
 import com.mungwin.payngwinteller.security.service.aspects.ConsoleAuthHandler;
 import com.mungwin.payngwinteller.security.service.aspects.PreApprove;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("${api.base}/public/console")
@@ -25,5 +28,21 @@ public class ConsoleController {
     @PreApprove(handler = ConsoleAuthHandler.class)
     public ResponseEntity<ApiResponse<MoMoTokenDTO>> testMomoLogin() {
         return ResponseEntity.ok(ApiResponse.from(moMoRestClient.login()));
+    }
+    @GetMapping("/momo/requesttopay")
+    public ResponseEntity<Boolean> testRequestToPay() {
+        MoMoPayRequest moMoPayRequest = new MoMoPayRequest();
+        moMoPayRequest.setAmount("10");
+        moMoPayRequest.setCurrency("XAF");
+        moMoPayRequest.setExternalId("12345");
+        MoMoPayer payer = new MoMoPayer();
+        payer.setPartyId("237653954068");
+        payer.setPartyIdType("MSISDN");
+        moMoPayRequest.setPayer(payer);
+        moMoPayRequest.setPayerMessage("Collection for CNPS");
+        moMoPayRequest.setPayeeNote("Collection for CNPS");
+        String referenceId = UUID.randomUUID().toString();
+        return ResponseEntity.ok(moMoRestClient.requestToPay(
+                moMoPayRequest, referenceId, moMoRestClient.login().getAccess_token()));
     }
 }
